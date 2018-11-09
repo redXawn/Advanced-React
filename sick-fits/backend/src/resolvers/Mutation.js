4ef5c05195a49c4bb81242f8806e01bf131f30aa
andrew <sbstn.andrew56@gmail.com>
2018-11-09 13:35:23 +0700

    finish course 12

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const mutations = {};
+const Mutations = {
+  createDog(parent, args, context, info) {
+    global.dogs = global.dogs || [];
+    // create a dog!
+    const newDog = { name: args.name };
+    global.dogs.push(newDog)
+    return newDog
+    console.log(args)
+  }
+};
 
-module.exports = mutations;
+module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,9 @@
-const Query = {};
+const Query = {
+  dogs(parent, args, context, info) {
+    global.dogs = global.dogs || [];
+    console.log('dogs', global.dogs)
+    return global.dogs;
+  }
+};
 
 module.exports = Query;
